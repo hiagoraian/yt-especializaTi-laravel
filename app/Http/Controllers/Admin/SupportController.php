15bb42f698e8e625245c8f4417750fec90c0f666
hiagoraian <hiagogoncalves@proton.me>
2023-05-31 22:24:17 -0300

    Iniciando DTOS

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupportRequest;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller{
-    public function index(Support $support){
 
-        $supports = $support->all();
-        # dd($supports); Var_dump
+    public function __construct(
+        protected SupportService $service
+    ) {}
 
+
+    public function index(Request $request){
+
+        $supports = $this->service->getAll($request->filter);
+       
         return view('admin.supports.index', compact('supports'));
     }
 
@@ -32,9 +38,9 @@ class SupportController extends Controller{
 
     }
 
-    public function show(string|int $id){
+    public function show(string $id){
 
-        if(!$support = Support::find($id)){
+        if(!$support = $this->service->findOne($id)){
             return redirect()->back();
         }
 
@@ -43,8 +49,9 @@ class SupportController extends Controller{
 
     }
 
-    public function edit(Support $support ,string|int $id ){
-        if(!$support = $support->where('id',$id)->first()){
+    public function edit(string $id ){
+       // if(!$support = $support->where('id',$id)->first()){
+        if(!$support = $this->service->findOne($id)){
             return back();
         }
         return view('admin.supports.edit', compact('support'));
@@ -70,12 +77,9 @@ class SupportController extends Controller{
 
     }
 
-    public function destroy (string|int $id){
-        if(!$support = Support::find($id)){
-            return back();
-        }
-
-        $support->delete();
+    public function destroy (string $id){
+         $this->service->delete($id);
+        
         return redirect()->route('supports.index');
     }
 }
